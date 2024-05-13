d6ee3d6b021274936fcf751da75b86b1ac9b5b64
Rahfi <108726715+rhfnx@users.noreply.github.com>
2024-05-13 10:36:06 +0700

    Update tb1_pemrograman_lanjut.py

--- tb1_pemrograman_lanjut.py
+++ tb1_pemrograman_lanjut.py
@@ -5,33 +5,48 @@ NIM: 41822010038
 
 import re
 
-def get_input_matrix():
-    n, m = map(int, input().rstrip().split())
-    matrix = [input() for _ in range(n)]
+def get_input_matrix(filename):
+  try:
+    with open(filename, 'r') as f:
+      n, m = map(int, f.readline().rstrip().split())
+      matrix = [line.rstrip() for line in f]
     return matrix, m
+  except FileNotFoundError:
+    print(f"Error: File '{filename}' not found.")
+    exit()
 
 def decode_message(matrix, m):
-    decode_msg = ""
-    for i in range(m):
-        for j in range(len(matrix)):
-            try:
-                decode_msg += matrix[j][i]
-            except IndexError:
-                pass
-    return decode_msg
+  decode_msg = ""
+  for i in range(m):
+    for j in range(len(matrix)):
+      try:
+        decode_msg += matrix[j][i]
+      except IndexError:
+        decode_msg += ' '
+  return decode_msg
 
 def clean_message(decode_msg):
-    pattern = r'(?<=[\w])[^\w]+(?=[\w])'
-    match_msg = re.findall(pattern, decode_msg)
-    for x in match_msg:
-        decode_msg = decode_msg.replace(x, ' ', 1)
-    return decode_msg
+  pattern = r'(?<=[\w])[^\w]+(?=[\w])'
+  match_msg = re.findall(pattern, decode_msg)
+  for x in match_msg:
+    decode_msg = decode_msg.replace(x, ' ', 1)
+  return decode_msg
 
 def main():
-    matrix, m = get_input_matrix()
+  filename = 'text.txt'
+  try:
+    matrix, m = get_input_matrix(filename)
     decoded_message = decode_message(matrix, m)
-    cleaned_message = clean_message(decoded_message)
-    print(cleaned_message)
+    cleaned_message = clean_message((decoded_message))
+
+    resultfile = open("result.txt","w")
+    
+    resultfile.write(cleaned_message)
+    resultfile.close()
+    
+    print(f"Decoded Message:\n{cleaned_message}\nThe result is writen in result.txt!")
+  except Exception as e:
+    print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    main()
+  main()
